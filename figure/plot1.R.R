297c9e91523e6b7fbe6b60d779b40ed2fb4c77c0
Mohamed  TANANI <mtanani@insea.ac.ma>
2021-04-19 23:53:44 +0000

    Creating 4 plots

---
+++ figure/plot1.R.R
@@ -0,0 +1,22 @@
+# Reading data into R from the current working directory
+data=read.table("household_power_consumption.txt",header=T,sep=";")
+
+# Subsetting the data to create new one that contains the dates from 2007-02-01 to 2007-02-02 
+pos1=which(data[,1]=="1/2/2007"); pos1=pos1[1]; pos1
+pos2=which(data[,1]=="2/2/2007"); pos2=pos2[length(pos2)]; pos2
+data=data[pos1:pos2,]
+
+# Converting the Date column from chr to date 
+data$Date=as.Date(data$Date,"%d/%m/%Y")
+
+# Converting the Time column from chr to time 
+library(hms)
+data$Time=as_hms(data$Time)
+
+# Converting the other columns from chr to num
+for(i in 3:ncol(data)){data[,i]=as.numeric(data[,i])}
+
+# Plot1
+png(file="plot1.png",width=480,height=480)
+hist(data$Global_active_power,col="red",main="Global Active Power",xlab="Global Active Power (kilowatts)")
+dev.off()
---
+++ figure/plot4.R
@@ -0,0 +1,29 @@
+# Reading data into R from the current working directory
+data=read.table("household_power_consumption.txt",header=T,sep=";")
+
+# Subsetting the data to create new one that contains the dates from 2007-02-01 to 2007-02-02 
+pos1=which(data[,1]=="1/2/2007"); pos1=pos1[1]; pos1
+pos2=which(data[,1]=="2/2/2007"); pos2=pos2[length(pos2)]; pos2
+data=data[pos1:pos2,]
+
+# Converting the Date column from chr to date 
+data$Date=as.Date(data$Date,"%d/%m/%Y")
+
+# Converting the Time column from chr to time 
+library(hms)
+data$Time=as_hms(data$Time)
+
+# Converting the other columns from chr to num
+for(i in 3:ncol(data)){data[,i]=as.numeric(data[,i])}
+
+# Plot4
+png(file="plot4.png",width=480,height=480)
+par(mfrow=c(2,2))
+plot(ymd(data$Date)+hms(data$Time),data$Global_active_power,type="l",xlab="",ylab="Global Active Power")
+plot(ymd(data$Date)+hms(data$Time),data$Voltage,type="l",xlab="datetime",ylab="Voltage")
+plot(ymd(data$Date)+hms(data$Time),data$Sub_metering_1,ylab="Energy sub metering",xlab="",type="l")
+lines(ymd(data$Date)+hms(data$Time),data$Sub_metering_2,col="red")
+lines(ymd(data$Date)+hms(data$Time),data$Sub_metering_3,col="blue")
+legend("topright",col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_1"),lwd=3)
+plot(ymd(data$Date)+hms(data$Time),data$Global_reactive_power,type="l",xlab="datetime",ylab="Global_reactive_power")
+dev.off()
